05903dc75f3c7ba37a4ae17c4c7edaf099e166dd
KrotVidit <backendkid@mail.ru>
2023-01-30 17:29:10 +0300

    testCase

--- src/main/java/org/academyTop/LoginPassword.java
+++ src/main/java/org/academyTop/LoginPassword.java
@@ -1,63 +1,83 @@
 package org.academyTop;
 
-
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class LoginPassword  {
+public class LoginPassword {
     Scanner scanner = new Scanner(System.in);
-    Workbook workbook = new HSSFWorkbook(new FileInputStream("D:\\Java\\IDEA\\Examination paper\\company.xls"));
+    String separator = File.separator;
+    FileInputStream fileExel = new FileInputStream("." + separator + "src" + separator + "main" + separator + "resources" + separator + "company.xls");
+    Workbook workbook = new HSSFWorkbook(fileExel);
+
+    HashMap<String, Integer> hashLoginPassword = new HashMap<>();
 
     public LoginPassword() throws IOException {
     }
 
-    private String readsLogin(){
+
+    private String readsLogin() {
         System.out.println("Enter Login");
-       return scanner.next();
+        return scanner.next();
     }
 
-    private int readsPassword(){
+    private int readsPassword() {
         System.out.println("Enter Password");
         return scanner.nextInt();
     }
 
-    private void printNotCorrect(){
-        System.out.println("Incorrect data entered\nTry again \n");
-        checkLoginPassword();
+    public void putLoginPasswordHashMap() throws IOException {
+
+
+        int starRow = 1;
+        int endRow = 10;
+        String login = null;
+        int password = 0;
+        for (int i = starRow; i < endRow; i++) {
+            login = String.valueOf(workbook.getSheetAt(1).getRow(i).getCell(3));
+            password = (int) workbook.getSheetAt(1).getRow(i).getCell(4).getNumericCellValue();
+
+            hashLoginPassword.put(login, password);
+        }
+
+
+        fileExel.close();
     }
 
 
-
-
-
-    private void checkLoginPassword(){
-       String login = workbook.getSheetAt(1).getRow(1).getCell(3).getStringCellValue();
-       if (login.equals(readsLogin())){
-           System.out.println("Correct Login");
-       }
-       else {
-           printNotCorrect();
-       }
-
-       int password = (int) workbook.getSheetAt(1).getRow(1).getCell(4).getNumericCellValue();
-
-       if(password==readsPassword()){
-           System.out.println("Correct Password");
-       }
-       else {
-           printNotCorrect();
-       }
-       new Menu().getPrintMenuAdmin();
+    //    public boolean checkLoginPassword() {
+//        String login = readsLogin();
+//        Integer password = hashLoginPassword.get(login);
+//        if (password == null) {
+//            return false;
+//        } else {
+//            System.out.println("win");
+//            int printPassword = readsPassword();
+//            password = hashLoginPassword.get(printPassword);
+//            if (password == null) {
+//                checkLoginPassword();
+//                return false;
+//            } else {
+//                System.out.println("Win");
+//                return true;
+//            }
+//        }
+//
+//    }
+    private void checkLoginPassword() {
 
     }
 
-    public void getCheckLoginPassword(){
+    public void getCheckLoginPassword() {
         checkLoginPassword();
     }
 
 
 }
+
+
--- src/main/java/org/academyTop/Main.java
+++ src/main/java/org/academyTop/Main.java
@@ -4,11 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new Menu().getPrintsNameProgram();
-        new LoginPassword().getCheckLoginPassword();
-
-
-
+       new LoginPassword().getCheckLoginPassword();
 
 
     }
