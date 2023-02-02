47bf2807125d9a35315fc35eda4adbdd35e14c4d
KrotVidit <backendkid@mail.ru>
2023-02-03 00:15:28 +0300

    Fix DateBase

--- src/main/java/org/academyTop/DateBase.java
+++ src/main/java/org/academyTop/DateBase.java
@@ -1,10 +1,14 @@
 package org.academyTop;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class DateBase {
 
@@ -18,6 +22,50 @@ public class DateBase {
         return patchFileExel();
     }
 
+
+    private void All() throws IOException {
+        for (Row row : patchFileExel().getSheetAt(0)) {
+            for (Cell cell : row) {
+
+                System.out.println(ChecksCellForFormat(cell) + " ");
+            }
+        }
+        patchFileExel().close();
+    }
+    public void getAll() throws IOException {
+        All();
+    }
+
+
+    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+    private static String ChecksCellForFormat(Cell cell) {
+        String result = null;
+        switch (cell.getCellType()) {
+            case STRING:
+                result = cell.getRichStringCellValue().getString();
+                break;
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    result = simpleDateFormat.format(cell.getDateCellValue()); // передали формат даты
+                } else {
+                    result = String.valueOf(cell.getNumericCellValue());
+                }
+                break;
+            case BOOLEAN:
+                result = String.valueOf(cell.getBooleanCellValue());
+                break;
+            case FORMULA:
+                result = cell.getCellFormula();
+                break;
+            default:
+                System.out.println();
+        }
+        return result;
+
+    }
+
 }
+
 
 
--- src/main/java/org/academyTop/Main.java
+++ src/main/java/org/academyTop/Main.java
@@ -4,13 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Menu menu =new Menu();
-        LoginPassword loginPassword = new LoginPassword();
-
-        menu.getPrintsNameProgram();
-        loginPassword.getPutLoginPasswordInHashMap();
-        loginPassword.getCheckLoginPassword();
-
+       new DateBase().getAll();
 
     }
 }
