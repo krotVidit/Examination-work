9b428ac6bfc3bb0c5d97c15150a0e43b81964ce5
KrotVidit <backendkid@mail.ru>
2023-02-17 12:11:32 +0300

    report money

--- src/main/java/org/academyTop/Report/ReportGenerator.java
+++ src/main/java/org/academyTop/Report/ReportGenerator.java
@@ -2,50 +2,80 @@ package org.academyTop.Report;
 
 import org.academyTop.DataBase;
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ReportGenerator {
-    private ArrayList<ArrayList<String>> data;
+    private List<Employee> employees;
 
-    public ReportGenerator() throws IOException {
-        DataBase database = new DataBase();
-        this.data = database.getPutDataBaseExelInArrayList();
+    public ReportGenerator(List<Employee> employees) {
+        this.employees = employees;
     }
 
-    public void getReport() {
-        List<Employee> employees = new EmployeeParser().parseData(data);
+    public Map<String, Double> getAverageSalaryByDepartment() {
+        Map<String, Double> departmentSalaryMap = new HashMap<>();
         Map<String, List<Employee>> employeesByDepartment = employees.stream()
                 .collect(Collectors.groupingBy(Employee::getDepartmentName));
-
-        System.out.println("Средняя зарплата по организации: " + getAverageSalary(employees));
-
         for (Map.Entry<String, List<Employee>> entry : employeesByDepartment.entrySet()) {
-            String department = entry.getKey();
             List<Employee> departmentEmployees = entry.getValue();
-            System.out.println("Средняя зарплата по отделу " + department + ": " + getAverageSalary(departmentEmployees));
+            double totalSalary = 0;
+            for (Employee employee : departmentEmployees) {
+                totalSalary += Double.parseDouble(employee.getSalary().replaceAll("[^\\d.]", ""));
+            }
+            double averageSalary = totalSalary / departmentEmployees.size();
+            departmentSalaryMap.put(entry.getKey(), averageSalary);
         }
+        return departmentSalaryMap;
     }
 
-    private double getAverageSalary(List<Employee> employees) {
-        double totalSalary = employees.stream()
-                .mapToDouble(employee -> Double.parseDouble(employee.getSalary().replaceAll("[^\\d.]+", "")))
-                .sum();
+    public double getAverageSalary() {
+        double totalSalary = 0;
+        for (Employee employee : employees) {
+            totalSalary += Double.parseDouble(employee.getSalary().replaceAll("[^\\d.]", ""));
+        }
         return totalSalary / employees.size();
     }
 
-    public static void main(String[] args) {
-        try {
-            ReportGenerator reportGenerator = new ReportGenerator();
-            reportGenerator.getReport();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void generateReport() {
+        Map<String, Double> averageSalaryByDepartment = getAverageSalaryByDepartment();
+        double averageSalary = getAverageSalary();
+
+        System.out.println("Средняя зарплата по организации: " + averageSalary);
+        for (Map.Entry<String, Double> entry : averageSalaryByDepartment.entrySet()) {
+            String department = entry.getKey();
+            double departmentAverageSalary = entry.getValue();
+            System.out.println("Средняя зарплата по отделу " + department + ": " + departmentAverageSalary);
         }
     }
+
+    public void saveReportToFile(String fileName) throws IOException {
+        Map<String, Double> averageSalaryByDepartment = getAverageSalaryByDepartment();
+        double averageSalary = getAverageSalary();
+
+        FileWriter fileWriter = new FileWriter(fileName);
+        fileWriter.write("Средняя зарплата по организации: " + averageSalary + "\n");
+        for (Map.Entry<String, Double> entry : averageSalaryByDepartment.entrySet()) {
+            String department = entry.getKey();
+            double departmentAverageSalary = entry.getValue();
+            fileWriter.write("Средняя зарплата по отделу " + department + ": " + departmentAverageSalary + "\n");
+        }
+        fileWriter.close();
+        System.out.println("Отчёт успешно сохранён в файл " + fileName);
+    }
+
+    public static void main(String[] args) throws IOException {
+        DataBase database = new DataBase();
+        RoleAssigner roleAssigner = new RoleAssigner();
+        EmployeeParser employeeParser = new EmployeeParser();
+        List<Employee> employees = employeeParser.parseData(roleAssigner.getData());
+
+        ReportGenerator reportGenerator = new ReportGenerator(employees);
+        reportGenerator.generateReport();
+        reportGenerator.saveReportToFile("report.txt");
+    }
 }
-
-
 
